85597d5e5c04ef7c8aca57693e4d86af719bfb35
= <=>
2023-01-14 20:53:22 -0400

    eureka server config init

---
+++ src/test/java/io/github/credcardtraing/eurekaServe/EurekaServeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.credcardtraing.eurekaServe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
