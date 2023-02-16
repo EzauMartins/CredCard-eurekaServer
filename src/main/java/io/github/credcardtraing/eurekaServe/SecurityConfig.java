8833c121587b4c7c41fec8649ece2d2ae0740ac8
= <=>
2023-02-15 21:59:11 -0400

    config spring security

--- src/main/java/io/github/credcardtraing/eurekaServe/SecurityConfig.java
+++ src/main/java/io/github/credcardtraing/eurekaServe/SecurityConfig.java
@@ -1,2 +1,21 @@
-package io.github.credcardtraing.eurekaServe;public class SecurityConfig {
+package io.github.credcardtraing.eurekaServe;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
 }
